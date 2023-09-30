8d4913f5e1c91436d41d505907be56456cbef2e7
Andrew Chung <aandrewchung@gmail.com>
2023-09-30 16:04:34 -0400

    exported endpt fns for devs to use

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Web3Card from './components/Web3Card';
+import { 
+  fetchUniversalProfile, 
+  fetchENSProfile,
+  fetchLensProfile,
+  fetchFarcasterProfile,
+  fetchDotBitProfile 
+} from './apiFunctions';
 
 // Export the Web3Card component for use in other applications
 export default Web3Card;
+
+// Export the endpt functions for use in other applications
+export {
+  fetchUniversalProfile, 
+  fetchENSProfile,
+  fetchLensProfile,
+  fetchFarcasterProfile,
+  fetchDotBitProfile
+};
 
 // Render the example application
 ReactDOM.render(
