949e19f965c4bd0966921720d2979f61e5e843aa
Andrew Chung <aandrewchung@gmail.com>
2023-10-02 16:23:49 -0400

    fixed endpt url for lens profile

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -23,7 +23,7 @@ export const fetchENSProfile = async (identity) => {
 
 export const fetchLensProfile = async (identity) => {
     try {
-      const response = await axios.get(`https://api.web3.bio/profile/ens/${identity}`);
+      const response = await axios.get(`https://api.web3.bio/profile/lens/${identity}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching ENS profile:', error);
